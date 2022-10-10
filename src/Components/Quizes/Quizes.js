5b27335783eeec5e9078ee71933afd583400fd2d
katibur <katiburrahmansany@gmail.com>
2022-10-11 01:57:13 +0600

    quiz section added on home page

--- src/Components/Quizes/Quizes.js
+++ src/Components/Quizes/Quizes.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+
+import Card from 'react-bootstrap/Card';
 
 const Quizes = ({ quizes }) => {
 
     const { name, logo, total } = quizes;
     return (
-        <div>
-            <h2>{name}</h2>
-            <img src={logo} alt="" />
-            <p>{total}</p>
+        <div className='quizes-container'>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={logo} />
+                <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Text>
+                        {total}
+                    </Card.Text>
+                    <Button variant="primary">Check Yourself</Button>
+                </Card.Body>
+            </Card>
         </div>
     );
 };
