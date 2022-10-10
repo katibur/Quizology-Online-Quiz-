20cde27b49de4bdb0634408c99000ce6bbbc591a
katibur <katiburrahmansany@gmail.com>
2022-10-11 00:26:52 +0600

    blog section added

--- src/App.js
+++ src/App.js
@@ -28,6 +28,10 @@ function App() {
           element: <Blog></Blog>
         }
       ]
+    },
+    {
+      path: '*',
+      element: <div><h1>error 404</h1><h2>Page Not Found.</h2></div>
     }
   ]);
   return (
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-const Header = () => {
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeadSideVirus } from '@fortawesome/free-solid-svg-icons'
+
+function Header() {
     return (
         <div>
-            <Link to='/'>Home</Link>
-            <Link to='/statistics'>Statistics</Link>
-            <Link to='/blog'>Blog</Link>
+            <div>
+                <Navbar bg="info" expand="lg" sticky='top' className='py-3'>
+                    <Container>
+                        <div>
+                            <Navbar.Brand href="#home" className='bg-secondary px-4 py-2 rounded text-white fw-bold'><FontAwesomeIcon icon={faHeadSideVirus} /> Quizology For You <FontAwesomeIcon icon={faHeadSideVirus} /></Navbar.Brand>
+                        </div>
+                        <div>
+                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                            <Navbar.Collapse id="basic-navbar-nav">
+                                <Nav className="mx-auto fw-bold">
+                                    <Nav.Link href="/" className='text-white'>Home</Nav.Link>
+                                    <Nav.Link href="/statistics" className='text-white'>Statistics</Nav.Link>
+                                    <Nav.Link href="/Blog" className='text-white'>Blog</Nav.Link>
+                                </Nav>
+                            </Navbar.Collapse>
+                        </div>
+                    </Container>
+                </Navbar>
+            </div>
+            <div>
+                <h2>Why?</h2>
+            </div>
         </div>
     );
-};
+}
 
 export default Header;
